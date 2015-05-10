3cab6a2ac3e8d76c733a046f2c19d0a46ad04c60
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-10 15:22:52 -0500

    Gallery

--- media/galleries/gallerymenu.php
+++ media/galleries/gallerymenu.php
@@ -1,13 +1,14 @@
-									<li><a><strong>2012-2013</strong></a></li>
-									<li><a href="index.php?gallery=buildseason&amp;title=Build%20Season">Build Season</a></li>
-									<li><a href="index.php?gallery=sciencenight&amp;title=Science%20Night">Science Night</a></li>
-									<li><a href="index.php?gallery=fallfest&amp;title=Fall%20Fest%20and%20Homecoming%20Carnival">Fall Fest and Homecoming Carnival</a></li>
-									<li><a href="index.php?gallery=massivemini&amp;title=Massive%20Mini">Massive Mini</a></li>
-									<li><a href="index.php?gallery=rebuilding&amp;title=Rebuilding%20Together%20Clay%20County">Rebuilding Together Clay County</a></li>
-									<li><a href="index.php?gallery=highway&amp;title=Highway%20Cleanup">Highway Cleanup</a></li>
-									<li class="divider"></li>
-									<li><a><strong>2011-2012</strong></a></li>
-									<li><a href="index.php?gallery=kcregional20112012&amp;title=2011-2012%20Kansas%20City%20Regional">Kansas City Regional</a></li>
-									<li><a href="index.php?gallery=coloradoregional20112012&amp;title=2011-2012%20Colorado%20Regional">Colorado Regional</a></li>
-									<li><a href="index.php?gallery=championships20112012&amp;title=2011-2012%20Champions">Championships</a></li>
-									
+<li><a><strong>2012-2013</strong></a></li>
+<li><a href="index.php?gallery=buildseason&amp;title=Build%20Season">Build Season</a></li>
+<li><a href="index.php?gallery=sciencenight&amp;title=Science%20Night">Science Night</a></li>
+<li><a href="index.php?gallery=fallfest&amp;title=Fall%20Fest%20and%20Homecoming%20Carnival">Fall Fest and Homecoming Carnival</a></li>
+<li><a href="index.php?gallery=massivemini&amp;title=Massive%20Mini">Massive Mini</a></li>
+<li><a href="index.php?gallery=rebuilding&amp;title=Rebuilding%20Together%20Clay%20County">Rebuilding Together Clay County</a></li>
+<li><a href="index.php?gallery=highway&amp;title=Highway%20Cleanup">Highway Cleanup</a></li>
+<li class="divider"></li>
+<li><a><strong>2011-2012</strong></a></li>
+<li><a href="index.php?gallery=kcregional20112012&amp;title=2011-2012%20Kansas%20City%20Regional">Kansas City Regional</a></li>
+<li><a href="index.php?gallery=coloradoregional20112012&amp;title=2011-2012%20Colorado%20Regional">Colorado Regional</a></li>
+<li><a href="index.php?gallery=championships20112012&amp;title=2011-2012%20Champions">Championships</a></li>
+<li><a><strong>Gaming</strong></a></li>
+<li><a href="index.php?gallery=borderlands2&amp;title=Borderlands%202">Borderlands 2</a></li>
