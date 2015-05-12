f4fa75c91118a74f4c57200043419fd6b26eb6ef
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-12 17:18:03 -0500

    Sponsors

--- ourteam/sponsorlist/sponsorlist.php
+++ ourteam/sponsorlist/sponsorlist.php
@@ -14,53 +14,39 @@
 		<div class="page-header">
 			<h1><?php echo $pageTitle?></h1>
 		</div>
-		  <div class="featurette" id="1">
-				<a target="_blank" href="http://www.kcstem.org/"><img class="featurette-image pull-right" src="/bootstrap/img/logo_kc-stem-main.png" width="300" alt="KC STEM Alliance"></a>
-				<a target="_blank" href="http://www.kcstem.org/"><h2 class="featurette-heading">KC STEM Alliance</h2></a>
-		  </div>
-		  <hr class="featurette-divider">
+			<hr class="featurette-divider">
 		  <div class="featurette featurette-right" id="2">
-				<a target="_blank" href="http://www.liberty.k12.mo.us/"><img class="featurette-image pull-left" src="/bootstrap/img/lps.png" width="300" alt="Liberty Public Schools"></a>
+				<a target="_blank" href="http://www.liberty.k12.mo.us/"><img class="featurette-image pull-left" src="/bootstrap/img/sponsors/lps.png" width="300" alt="Liberty Public Schools"></a>
 				<a target="_blank" href="http://www.liberty.k12.mo.us/"><h2 class="featurette-heading">Liberty Public Schools</h2></a>
 		  </div>
 		  <hr class="featurette-divider">
 			<div class="featurette" id="3">
-				<a target="_blank" href="http://libertyroboticsfoundation.org/"><img class="featurette-image pull-right" src="/bootstrap/img/libertyroboticsfoundation.png" width="300" alt="Liberty Robotics Foundation"></a>
+				<a target="_blank" href="http://libertyroboticsfoundation.org/"><img class="featurette-image pull-right" src="/bootstrap/img/sponsors/foundation.png" width="300" alt="Liberty Robotics Foundation"></a>
 				<a target="_blank" href="http://libertyroboticsfoundation.org/"><h2 class="featurette-heading">Liberty Robotics Foundation</h2></a>
 		  </div>
-		  <hr class="featurette-divider">
-			<div class="featurette featurette-right" id="4">
-				<a target="_blank" href="http://www.argusconsulting.com/"><img class="featurette-image pull-left" src="/bootstrap/img/argus.png" width="300" alt="Argus Consulting"></a>
-				<a target="_blank" href="http://www.argusconsulting.com/"><h2 class="featurette-heading">Argus Consulting</h2></a>
+		   <hr class="featurette-divider">
+			<div class="featurette" id="5">
+				<a target="_blank"  href="http://www.ci.liberty.mo.us/index.aspx?nid=55"><img class="featurette-image pull-right" src="/bootstrap/img/sponsors/.png" width="300"  alt="Police Department"></a>
+				<a target="_blank"  href="http://www.ci.liberty.mo.us/index.aspx?nid=55"><h2 class="featurette-heading">City of Liberty Police Dept.</h2></a>
 		  </div>
 		  <hr class="featurette-divider">
 			<div class="featurette" id="5">
-				<a target="_blank"  href="https://driveone4urschool.com/"><img class="featurette-image pull-right" src="/bootstrap/img/drive_one.png" width="300"  alt="Drive One 4 Ur School"></a>
+				<a target="_blank"  href="https://driveone4urschool.com/"><img class="featurette-image pull-right" src="/bootstrap/img/sponsors/drive_one.png" width="300"  alt="Drive One 4 Ur School"></a>
 				<a target="_blank"  href="https://driveone4urschool.com/"><h2 class="featurette-heading">Drive One 4 Ur School</h2></a>
 		  </div>
 		  <hr class="featurette-divider">
 			<div class="featurette featurette-right" id="6">
-				<a target="_blank"  href="http://www.rotary.org/"><img class="featurette-image pull-left" src="/bootstrap/img/rotary.png" width="300"  alt="Rotary"></a>
+				<a target="_blank"  href="http://www.rotary.org/"><img class="featurette-image pull-left" src="/bootstrap/img/sponsors/rotary.png" width="300"  alt="Rotary"></a>
 				<a target="_blank"  href="http://www.rotary.org/"><h2 class="featurette-heading">Rotary International</h2></a>
 		  </div>
 		  <hr class="featurette-divider">
 			<div class="featurette featurette" id="7">
-				<a target="_blank"  href="http://www.honeywell.com/"><img class="featurette-image pull-right" src="/bootstrap/img/honeywell.png" width="300"  alt="Honeywell"></a>
+				<a target="_blank"  href="http://www.honeywell.com/"><img class="featurette-image pull-right" src="/bootstrap/img/sponsors/honeywell.png" width="300"  alt="Honeywell"></a>
 				<a target="_blank"  href="http://www.honeywell.com/"><h2 class="featurette-heading">Honeywell</h2></a>
 		  </div>
 		  <hr class="featurette-divider">
-			<div class="featurette featurette-right" id="8">
-				<a target="_blank"  href="http://www.lairdplastics.com/"><img class="featurette-image pull-left" src="/bootstrap/img/laird.png" width="300"  alt="Laird Plastics"></a>
-				<a target="_blank"  href="http://www.lairdplastics.com/"><h2 class="featurette-heading">Laird Plastics</h2></a>
-		  </div>
-		  <hr class="featurette-divider">
-			<div class="featurette" id="9">
-				<a target="_blank"  href="http://www.proactgroup.com/"><img class="featurette-image pull-right" src="/bootstrap/img/proact.png" width="300"  alt="Proact Marketing Page"></a>
-				<a target="_blank"  href="http://www.proactgroup.com/"><h2 class="featurette-heading">Proact Marketing Group</h2></a>
-		  </div>
-		  <hr class="featurette-divider">
 			<div class="featurette featurette-right" id="10">
-				<a target="_blank"  href="http://www.garycrossleyford.com/"><img class="featurette-image pull-left" src="/bootstrap/img/crossley.png" width="300"  alt="Gary Crossley Ford"></a>
+				<a target="_blank"  href="http://www.garycrossleyford.com/"><img class="featurette-image pull-left" src="/bootstrap/img/sponsors/crossley.png" width="300"  alt="Gary Crossley Ford"></a>
 				<a target="_blank"  href="http://www.garycrossleyford.com/"><h2 class="featurette-heading">Gary Crossley Ford - Drive One</h2></a>
 		  </div>
         </div><!--/span-->
