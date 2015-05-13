0760084bfe20ecaaf2bfedcabf7769431293f1f3
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-13 10:40:59 -0500

    Test Admin

---
+++ admin/admin.php
@@ -0,0 +1,26 @@
+	<div class="container-fluid marketing">
+    <div class="container">
+      <div class="row-fluid">
+	  <div data-spy="affix" class="span3 bs-docs-sidebar">
+		<ul class="nav nav-list bs-docs-sidenav">
+		  <li><a href="#1"><i class="icon-chevron-right"></i>Pages</a></li>
+		</ul>
+	  </div>
+        <div class="offset3 span9">
+		<div class="page-header">
+			<h1><?php echo $pageTitle?></h1>
+		</div>
+		  <div class="featurette" id="1">
+				<h2 class="featurette-heading">Pages</h2>
+				<br/>
+				<li>
+					<p class="lead">Our Team</p>
+					<p class="lead">Team List</p>
+					<p class="lead">Sponsor List</p>
+					<p class="lead">Competitions</p>
+				</li>
+		  </div>
+        </div><!--/span-->
+      </div><!--/row-->
+    </div><!--/.fluid-container-->
+	</div><!--/Marketing-->
---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Administration";
+$prependPath = "/var/www/robotics";
+$prependCSSPath = null;
+
+include($prependPath."/base/head.html");
+include("admin.php");
+include($prependPath."/base/footer.html");
+?>
