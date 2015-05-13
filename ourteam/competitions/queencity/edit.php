51b90ee3f0ea2375155cdf0887b2bfac7d593f12
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-12 23:16:04 -0500

    Edit CSS

--- ourteam/competitions/queencity/edit.php
+++ ourteam/competitions/queencity/edit.php
@@ -1,4 +1,8 @@
-<?php
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/signin.css" rel="stylesheet"><?php
 session_start();
 $authorized = false;
 if(isset($_SESSION['auth']))
@@ -42,12 +46,15 @@ if(isset($_GET['loginpage']))
     }
  else {
         ?>
-            <link href="./css/signin.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.css" rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link href="./css/signin.css" rel="stylesheet">
 <form class="form-signin">
         <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputUser" class="sr-only">Username</label>
+        <input type="text" id="inputUser" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox">
