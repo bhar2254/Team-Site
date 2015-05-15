a17b1d3b6ab1c7d0e47d2cd93bf464d5ae37e720
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-14 19:53:43 -0500

    Index
    Actually added the modals…

--- ourteam/divisions/index.php
+++ ourteam/divisions/index.php
@@ -4,6 +4,7 @@ $prependPath = "/var/www/robotics";
 $prependCSSPath = null;
 
 include($prependPath."/base/head.html");
+include("modals.php");
 include("divisions.php");
 include($prependPath."/base/footer.html");
 ?>
