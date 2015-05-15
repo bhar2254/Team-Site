3df422a0c894d7b5925edaee19af8dfd2e2418c3
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-14 19:50:16 -0500

    Modal
    Much prettier division look.

---
+++ ourteam/divisions/modals.php
@@ -0,0 +1,13 @@
+<!-- Modals -->
+<!-- divison photograph -->
+<div id="imgLarge" class="modal hide fade" tabindex="-1" role="dialog" aria-hidden="true">
+	<div class="modal-header">
+		<h3>The Division of Power</h3>
+	</div>
+	<div class="modal-body">
+		<img title="Team 1764 Infrastructure" alt="Team 1764 Infrastructure" src="imgs/Team_Wheel_Infrastructure.jpg">
+	</div>
+	<div class="modal-footer">
+		<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+	</div>
+</div>
